3fa85542032d19c25cf8ae9ef2a3cbff24b98427
Antonio583-ctrl <ricardodelgado1617@gmail.com>
2025-06-20 23:28:33 -0400

    Update ContactosAdminRuta.ts

--- src/Rutas/ContactosAdminRuta.ts
+++ src/Rutas/ContactosAdminRuta.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { isAuthenticated, isAdmin,} from '../middlewares/auth.middleware';
 import { ContactosModelo } from '../models/ContactosModelo';
+import { formatFecha } from '../utils/formatFecha';
 
 const router = Router();
 const modelo = new ContactosModelo();
 
 router.get('/contacts', isAuthenticated, isAdmin, async (req, res) => {
   const contactos = await modelo.getAllContacts();
-  res.render('contacts', { contactos, title: 'Contactos - Administración' });
+  res.render('contacts', { contactos, formatFecha, title: 'Contactos - Administración' });
 });
 
 export default router;
