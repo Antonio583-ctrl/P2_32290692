2c265754b6973afc5199e05cf26032340a752d4a
Antonio583-ctrl <ricardodelgado1617@gmail.com>
2025-06-27 11:09:39 -0400

    Update formatMoneda.ts

--- src/helpers/formatMoneda.ts
+++ src/helpers/formatMoneda.ts
@@ -1,7 +1,18 @@
 export function formatMoneda(monto: number, lang: string) {
   if (lang === 'en') {
-    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(monto);
+    // Inglés (EE.UU.): USD $15.00
+    return `USD ${new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      currencyDisplay: 'symbol'
+    }).format(monto)}`;
   } else {
-    return new Intl.NumberFormat('es-VE', { style: 'currency', currency: 'VES' }).format(monto);
+    // Español (Venezuela): VES 1.500,00 Bs
+    const montoEnBs = monto * 106; // Conversión a céntimos
+    return `VES ${new Intl.NumberFormat('es-VE', {
+      style: 'decimal',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    }).format(montoEnBs)} Bs`;
   }
 }
